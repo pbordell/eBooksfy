bacd4b142126606c9d2d908561cbe401b39d8eeb
ALTRAN_ES\pere.bordellsarda <pere.bordellsarda@altran.com>
2019-03-08 09:52:24 +0100

    First commit

---
+++ springmvc-mongodb/src/main/java/com/pbs/eBooksfy/EBooksfyApplication.java
@@ -0,0 +1,13 @@
+package com.pbs.eBooksfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBooksfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBooksfyApplication.class, args);
+	}
+
+}
